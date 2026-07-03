a55f4d3ad3db8a2270ca82b93dd58bc7485b75cc
oyyj0101 <b1a2i3j4i5u6@gmail.com>
2026-07-03 16:07:53 +0800

    Fix: Force table mapping to upperrcase Post in DbContext

--- BulletinBoardContext.cs
+++ BulletinBoardContext.cs
@@ -3,51 +3,25 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using BulletinBoard.Entities;
+using BulletinBoard.Models;
 
-namespace BulletinBoard
+namespace BulletinBoard.Entities
 {
-    public partial class BulletinBoardContext : DbContext
+    public class BulletinBoardContext : DbContext
     {
-        public BulletinBoardContext()
+        public BulletinBoardContext(DbContextOptions<BulletinBoardContext> options) : base(options)
         {
         }
 
-        public BulletinBoardContext(DbContextOptions<BulletinBoardContext> options)
-            : base(options)
-        {
-        }
+        public DbSet<Post> Post { get; set; } = null!;
 
-        public virtual DbSet<Post> Post { get; set; } = null!;
-
+        // 🎯 確保這段程式碼有躺在你的 Context 檔案裡
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.UseCollation("utf8mb4_0900_ai_ci")
-                .HasCharSet("utf8mb4");
+            base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Post>(entity =>
-            {
-                entity.ToTable("post");
-
-                entity.HasComment("貼文");
-
-                entity.Property(e => e.Id)
-                    .HasColumnName("id")
-                    .HasComment("流水編號");
-
-                entity.Property(e => e.Content)
-                    .HasColumnType("text")
-                    .HasColumnName("content")
-                    .HasComment("內文");
-
-                entity.Property(e => e.Title)
-                    .HasMaxLength(100)
-                    .HasColumnName("title")
-                    .HasComment("標題");
-            });
-
-            OnModelCreatingPartial(modelBuilder);
+            // 強制定向大寫表格！
+            modelBuilder.Entity<Post>().ToTable("Post");
         }
-
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
