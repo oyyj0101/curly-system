851770316f70b34aef8349e2c94557bf77d32462
oyyj0101 <b1a2i3j4i5u6@gmail.com>
2026-07-03 16:13:23 +0800

    Fix: Force table mapping to upperrcase Post in DbContext

--- BulletinBoardContext.cs
+++ BulletinBoardContext.cs
@@ -3,25 +3,55 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using BulletinBoard.Entities;
-using BulletinBoard.Models;
 
-namespace BulletinBoard.Entities
+namespace BulletinBoard
 {
-    public class BulletinBoardContext : DbContext
+    public partial class BulletinBoardContext : DbContext
     {
-        public BulletinBoardContext(DbContextOptions<BulletinBoardContext> options) : base(options)
+        public BulletinBoardContext()
         {
         }
 
-        public DbSet<Post> Post { get; set; } = null!;
+        public BulletinBoardContext(DbContextOptions<BulletinBoardContext> options)
+            : base(options)
+        {
+        }
 
-        // 🎯 確保這段程式碼有躺在你的 Context 檔案裡
+        public virtual DbSet<Post> Post { get; set; } = null!;
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            // 強制定向大寫表格！
-            modelBuilder.Entity<Post>().ToTable("Post");
+    // 🎯 【就是這行大絕招】強制將 Post 實體對應到 PostgreSQL 中大寫的 "Post" 資料表
+    modelBuilder.Entity<Post>().ToTable("Post");
+            modelBuilder.UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.ToTable("post");
+
+                entity.HasComment("貼文");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasComment("流水編號");
+
+                entity.Property(e => e.Content)
+                    .HasColumnType("text")
+                    .HasColumnName("content")
+                    .HasComment("內文");
+
+                entity.Property(e => e.Title)
+                    .HasMaxLength(100)
+                    .HasColumnName("title")
+                    .HasComment("標題");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
         }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
