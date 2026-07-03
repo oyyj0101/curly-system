1de6914b548146c1cd9b268026997b6c6106797e
oyyj0101 <b1a2i3j4i5u6@gmail.com>
2026-07-03 16:18:15 +0800

    Fix: Force table mapping to upperrcase Post in DbContext

--- BulletinBoardContext.cs
+++ BulletinBoardContext.cs
@@ -3,6 +3,29 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using BulletinBoard.Entities;
+using Microsoft.EntityFrameworkCore;
+using BulletinBoard.Models; // 或是你的 Post 所在的命名空間
+
+namespace BulletinBoard.Entities
+{
+    public class BulletinBoardContext : DbContext
+    {
+        public BulletinBoardContext(DbContextOptions<BulletinBoardContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Post { get; set; } = null!;
+
+        // 🎯 確保這段程式碼有躺在你的 Context 檔案裡
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // 強制定向大寫表格！
+            modelBuilder.Entity<Post>().ToTable("Post");
+        }
+    }
+}
 
 namespace BulletinBoard
 {
